04a8031807bcc98fe408d125929afba4ee8cfc7c
Estudiante-10 <acelite27@gmail.com>
2021-11-17 20:21:56 -0600

    reparando consulta a base de datos

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -22,7 +22,7 @@ class UsersController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Show the form for creating a new resource
      *
      * @return \Illuminate\Http\Response
      */
